b24d7ecd8332f66cf8be24960dfda7f2086172bd
jaseel <jaseeljctcs@gmail.com>
2022-12-05 22:50:02 +0530

    admin

--- src/main/java/com/example/Library_backend_app/controller/LibraryController.java
+++ src/main/java/com/example/Library_backend_app/controller/LibraryController.java
@@ -1,4 +1,13 @@
 package com.example.Library_backend_app.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class LibraryController {
+    @PostMapping("/admin")
+    public String AdminLogin(){
+        return "Admin Login page";
+    }
 }
